9ca15c2cebe4f20975ead1eaba3d7423ec8c1986
Kartik Mahajan <karmah24@DESKTOP-EQB5R0Q.localdomain>
2021-05-16 22:55:17 +0530

    valid parentheses

---
+++ Stacks_Queues/valid_substring.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestValidParentheses(string str) {
+        
+        stack<int> s;
+        s.push(-1);
+        int st, res = 0;
+        
+        for (int i = 0; i < str.size(); i++) {
+            
+            if (str[i] == '(') s.push(i);
+            else {
+                s.pop();
+                if (s.empty()) s.push(i);
+                else {
+                    res = max(res, i - s.top());
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    string str;
+    cin >> str;
+    Solution obj;
+    cout << obj.longestValidParentheses(str);
+}
