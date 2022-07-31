2b42afb8f7aedaa85d7826632a03304eab0dd0d9
Karmah24 <kartikmahajan42116@gmail.com>
2022-07-31 22:42:56 +0530

    mac back

--- Dynamic_Programming/book_shop.cpp
+++ Dynamic_Programming/book_shop.cpp
@@ -1,46 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+ 
+int maxPages(int n, int x, vector<int> &price, vector<int> &pages) {
 
-// vector<vector<int>> dp;
-
-// int solve(int n, int W, int price[], int pages[]) {
-
-//     if (n < 0 || W == 0) return 0;
-//     if (dp[n][W] != -1) return dp[n][W];
-
-//     int ifin, ifout = solve(n - 1, W, price, pages);
-//     ifin = W < price[n] ? 0 : pages[n] + solve(n - 1, W - price[n], price, pages);
-//     return dp[n][W] = max(ifin, ifout);
-// }
-
-int maxPages(int n, int x, int price[], int pages[]) {
-
-    // dp = vector<vector<int>>(n, vector<int>(x + 1, -1));
-    // return solve(n - 1, x, price, pages);
-
-    int dp[2][x + 1];
-    memset(dp, 0, sizeof(dp));
-
+    vector<int> dp(x + 1, 0);
+ 
     for (int i = 0; i < n; i++) {
-
-        for (int j = 1; j <= x; j++) {
+ 
+        for (int j = x; j >= price[i]; j--) {
             
-            int ifout = dp[0][j];
-            int ifin = j < price[i] ? 0 : dp[0][j - price[i]] + pages[i];
-            dp[1][j] = max(ifin, ifout);
+            dp[j] = max(dp[j], dp[j - price[i]] + pages[i]);
         }
-        for (int j = 1; j <= x; j++) dp[0][j] = dp[1][j];
     }
-    return dp[0][x];
+    return dp[x];
 }
+ 
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    freopen("../input.txt", "r", stdin);
-    freopen("../output.txt", "w", stdout);
-
+ 
     int n, x;
     cin >> n >> x;
-    int price[n], pages[n];
+    vector<int> price(n), pages(n);
     for (int i = 0; i < n; i++) cin >> price[i];
     for (int i = 0; i < n; i++) cin >> pages[i];
     cout << maxPages(n, x, price, pages);
--- test.cpp
+++ test.cpp
@@ -1,40 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int solve() {
- 
-    int n, amt;
-    cin >> n >> amt;
-    int arr[n];
-    for (int i = 0; i < n; i++) cin >> arr[i];
- 
-    int dp[amt + 1];
-    memset(dp, -1, sizeof(dp));
-    dp[0] = 0;
- 
-    for (int i = 1; i <= amt; i++) {
- 
-        for (int j = 0; j < n; j++) {
- 
-            if (i < arr[j]) continue;
+#define int long long
+//================================================================
 
-            if (dp[i - arr[j]] == -1) continue;
-
-            if (dp[i] == -1) dp[i] = dp[i - arr[j]] + 1;
-            else dp[i] = min(dp[i], dp[i - arr[j]] + 1);
-        }
-    }
-    return dp[amt];
+void solve() {
+    
+    cout << "Karmah is back!!\n";
 }
- 
-int main() {
+signed main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    cout.setf(ios::fixed);
- 
+    // #ifndef ONLINE_JUDGE
+    //   freopen("input.txt", "r", stdin);
+    //   freopen("output.txt", "w", stdout);
+    // #endif
     int T = 1;
     // cin >> T;
     while (T--) {
-        cout << solve();
+        solve();
+        cout << '\n';
     }
     return 0;
 }
